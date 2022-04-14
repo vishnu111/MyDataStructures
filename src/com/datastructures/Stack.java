094447553e10486eef066c4f189eec523f76cf9e
Vishnu Lokesh Reddy <vishnulokesh520@gmail.com>
2022-04-15 01:01:21 +0530

    code for stack using arrays

--- src/com/datastructures/Stack.java
+++ src/com/datastructures/Stack.java
@@ -1,2 +1,10 @@
-package com.datastructures;public interface Stack {
+package com.datastructures;
+
+public interface Stack<E> {
+    E pop();
+    void push(E e);
+    boolean isEmpty();
+    E top();
+    int size();
+
 }
--- src/com/datastructures/StackUsingArrays.java
+++ src/com/datastructures/StackUsingArrays.java
@@ -1,13 +1,47 @@
 package com.datastructures;
 
-public class SingleLinkedList<E> implements Stack<E>{
+public class StackUsingArrays<E> implements Stack<E>{
     public static final int CAPACITY=100;
-    E[] e;
-    public SingleLinkedList(){
-        e=(E) new Object()[CAPACITY];
+    E[] data;
+    int t=-1;
+    public StackUsingArrays(){
+        this(CAPACITY);
+    }
+    public StackUsingArrays(int CAP){
+        data= (E[]) new Object[CAP];
     }
 
+    @Override
+    public E top() {
+        if(isEmpty()) return null;
+         return data[t];
+    }
+    public int size(){
+        return t+1;
+    }
+    public boolean isEmpty(){
+        return t==-1;
+    }
+    public void push(E e){
+        if(size()==data.length)throw new IllegalStateException("Stack is full");
+        data[++t]=e;
+    }
+    public E pop(){
+        if(isEmpty())return null;
+        E x=data[t];
+        data[t]=null;
+        t--;
+        return x;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        StackUsingArrays<Integer> sua= new StackUsingArrays<Integer>(5);
+        sua.push(1);
+        sua.push(2);
+        System.out.println(sua.pop());
+        sua.push(4);
+        System.out.println(sua.pop());
+        System.out.println(sua.size());
+        System.out.println(sua.top());
+        System.out.println(sua.isEmpty());
     }
 }
