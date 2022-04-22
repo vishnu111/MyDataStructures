a7ac6ab73aa837b990019f18071ec525ff83c945
Vishnu Lokesh Reddy <vishnulokesh520@gmail.com>
2022-04-23 00:23:59 +0530

    My leetcode problem on trees

--- src/com/hackerrank/StringMatching.java
+++ src/com/hackerrank/StringMatching.java
@@ -4,92 +4,118 @@ import java.util.*;
 import java.util.regex.*;
 class StringResult {
     public static String abbreviation(String a, String b) {
-        if(Pattern.compile(b, Pattern.CASE_INSENSITIVE).matcher(a).find())
+        String s = "(?=.*[A-Z]).+$";
+        String s1 = "(?=.*[a-z]).+$";
+        Pattern p = Pattern.compile(s);
+        Pattern p1 = Pattern.compile(s1);
+        /** p1.matcher(a.substring(Pattern.compile(b, Pattern.CASE_INSENSITIVE).matcher(a).end(), a.length())).matches()*/
+        /** p.matcher(charList.subList(1, upper).toString()).matches()*/
+        Pattern p2=Pattern.compile(b, Pattern.CASE_INSENSITIVE);
+        Matcher m2 = p2.matcher(a);
+        if(m2.find() && p1.matcher(a.substring(m2.end(),a.length())).matches() && !(p.matcher(a.substring(m2.end(),a.length())).matches()) ) {
             return "YES";
+        }
         else{
-            char[] charArray=a.toCharArray();
-            List<Character> charList= new ArrayList<>();
-            int flag=0;
-            String out="NO";
-            for(int i=0; i < charArray.length; i++){
-                if((flag==0) &&(Character.toLowerCase(charArray[i])==b.charAt(0) || Character.toUpperCase(charArray[i])==b.charAt(0)))
-                    flag=1;
-                if(flag==1)
+            char[] charArray = a.toCharArray();
+            List<Character> charList = new ArrayList<>();
+            int flag = 0;
+            String out = "NO";
+            for (int i = 0; i < charArray.length; i++) {
+                if ((flag == 0) && (Character.toLowerCase(charArray[i]) == b.charAt(0) || Character.toUpperCase(charArray[i]) == b.charAt(0)))
+                    flag = 1;
+                if (flag == 1)
                     charList.add(charArray[i]);
             }
-            int i=1;
-            String s = "(?=.*[A-Z]).+$";
-            Pattern p = Pattern.compile(s);
-            while(i<b.length() && charList.size()>0){
-                
-                /**checks if the string contains the next character we are looking for, if no return "NO"*/
-                if(charList.contains(b.charAt(i)) || charList.contains(Character.toLowerCase(b.charAt(i)))){
-                    
-                   /**getting the index of the found character, since we can consider both lower case and upper case, need to do some dirty work*/
-                   int upper=charList.size();
-                   if(charList.contains(Character.toLowerCase(b.charAt(i))))
-                       upper=charList.indexOf(Character.toLowerCase(b.charAt(i)));
-                   if(charList.contains(b.charAt(i))){
-                       if(charList.indexOf(b.charAt(i))<upper)
-                           upper=charList.indexOf(b.charAt(i));
-                   }
+            int i = 1;
 
-                    /**Now, we will check if the charecter in between the previos matching   
-                     * charecter and now found characters has any capitals in between, if "no" we will increment the i or 
+            while (i < b.length() && charList.size() > 0) {
+
+                /**checks if the string contains the next character we are looking for, if no return "NO"*/
+                if (charList.subList(1,charList.size()).contains(b.charAt(i)) || charList.subList(1,charList.size()).contains(Character.toLowerCase(b.charAt(i)))) {
+
+                    /**getting the index of the found character, since we can consider both lower case and upper case, need to do some dirty work*/
+                    int upper = charList.size();
+                    if (charList.subList(1,charList.size()).contains(Character.toLowerCase(b.charAt(i))))
+                        upper = charList.subList(1,charList.size()).indexOf(Character.toLowerCase(b.charAt(i)));
+                    if (charList.subList(1,charList.size()).contains(b.charAt(i))) {
+                        if (charList.subList(1,charList.size()).indexOf(b.charAt(i)) < upper)
+                            upper = charList.subList(1,charList.size()).indexOf(b.charAt(i));
+                    }
+                    upper++;
+                    System.out.println("The upper is "+upper);
+                    /**Now, we will check if the charecter in between the previos matching
+                     * charecter and now found characters has any capitals in between, if "no" we will increment the i or
                      * we will change the i to 1 to and remove the first some set of charecters till the matching char
                      */
-                    if(p.matcher(charList.subList(1,upper).toString()).matches()){
+                    if (p.matcher(charList.subList(1, upper).toString()).matches()) {
 
 
                         /**our sublist contains the capital letter, so we are now initializing out sublist to start at starting position of index*/
-                        if(charList.subList(1,upper).contains(b.charAt(i-1))){
+                        if (charList.subList(1, upper).contains(b.charAt(i - 1))) {
                             System.out.println("Here it is");
-                            charList.subList(0,charList.indexOf(b.charAt(i-1))).clear();
+                            charList.subList(0, charList.indexOf(b.charAt(i - 1))).clear();
                         }
 
                         /**Intilizes the charlist to starting matching index*/
-                        else{
-                            charList.subList(0,upper+1).clear();
-                            int lower=charList.size();
-                            int flag2=0;
-                            if(charList.contains(Character.toLowerCase(b.charAt(0)))) {
+                        else {
+                            charList.subList(0, upper + 1).clear();
+                            int lower = charList.size();
+                            int flag2 = 0;
+                            if (charList.contains(Character.toLowerCase(b.charAt(0)))) {
                                 lower = charList.indexOf(Character.toLowerCase(b.charAt(0)));
-                                flag2=1;
+                                flag2 = 1;
                             }
-                            if(charList.contains(b.charAt(0))){
-                                flag2=1;
-                                if(charList.indexOf(b.charAt(0))<lower)
-                                    lower=charList.indexOf(b.charAt(0));
+                            if (charList.contains(b.charAt(0))) {
+                                flag2 = 1;
+                                if (charList.indexOf(b.charAt(0)) < lower)
+                                    lower = charList.indexOf(b.charAt(0));
                             }
-                            if(flag2==1)
+                            if (flag2 == 1)
                                 return "NO";
-                            charList.subList(0,lower).clear();
+                            charList.subList(0, lower).clear();
                             System.out.println(charList.toString());
                         }
 
 
-                    }
-                    else{
-                        charList.subList(0,upper).clear();
+                    } else {
+                        charList.subList(0, upper).clear();
                         i++;
-                        if(i==b.length())
-                            return "YES";
+                        if (i == b.length()) {
+                            if (p.matcher(charList.subList(1, charList.size()).toString()).matches()) {
+                                int lower1 = charList.size();
+                                int flag1=0;
+                                if (charList.contains(Character.toLowerCase(b.charAt(0)))) {
+                                    lower1 = charList.indexOf(Character.toLowerCase(b.charAt(0)));
+                                    flag1=1;
+                                }
+                                if (charList.contains(b.charAt(0))) {
+                                    flag1=1;
+                                    if (charList.indexOf(b.charAt(0)) < lower1)
+                                        lower1 = charList.indexOf(b.charAt(0));
+                                }
+                                if(flag1==1)
+                                    charList.subList(0, lower1).clear();
+                                i=1;
+                            }
+                            else{
+                                return "YES";
+                            }
+                        }
                     }
-                }
-                else{
+                } else {
                     return "NO";
                 }
             }
             System.out.println(charList);
-        }
-        return "NO";
-    }
+            return "NO";
 
+        }
+    }
 }
 
 public class StringMatching {
     public static void main(String[] args) throws IOException {
-        String a ="beFgH",b="EFG";
+        String a ="LLZOSYAMQRMBTZXTQMQcKGLR",b="LLZOSYAMBTZXMQKLR";
                 System.out.println(StringResult.abbreviation(a,b));
     }
 }
---
+++ src/com/leetcode/trees/MorrisPreOrderTranversal.java
@@ -0,0 +1,46 @@
+package com.leetcode.trees;
+import java.util.*;
+class TreeNode {
+     int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+          this.left = left;
+         this.right = right;
+     }
+}
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        /**Morris travesal on pre-order*/
+        LinkedList<Integer> output= new LinkedList<>();
+        TreeNode node=root;
+        while(node != null){
+            if(node.left==null){
+                output.add(node.val);
+                node=node.right;
+            }
+            else{
+                TreeNode pred=node.left;
+                while(pred.right != null && pred.right != node){
+                    pred=pred.right;
+                }
+                if(pred.right==null){
+                    output.add(node.val);
+                    pred.right=node;
+                    node=node.left;
+                }
+                else{
+                    pred.right=null;
+                    node=node.right;
+                }
+            }
+        }
+        return output;
+    }
+}
+public class MorrisPreOrderTranversal {
+}
