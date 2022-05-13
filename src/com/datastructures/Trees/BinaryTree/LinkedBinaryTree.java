ae39c2f7eed9ad8d8cf8f2fdc49ff519c17f99dd
Vishnu Lokesh Reddy <vishnulokesh520@gmail.com>
2022-05-14 01:22:30 +0530

    level order tree traversal

--- src/com/datastructures/Trees/BinaryTree/LinkedBinaryTree.java
+++ src/com/datastructures/Trees/BinaryTree/LinkedBinaryTree.java
@@ -1,7 +1,7 @@
 package com.datastructures.Trees.BinaryTree;
 
 import com.datastructures.LinkedPositionalList.Position;
-
+import com.datastructures.Trees.BinaryTree.AbstractBinaryTree;
 public class LinkedBinaryTree<E> extends AbstractBinaryTree<E> {
     protected static class Node<E> implements Position<E>{
         private E element;
@@ -72,7 +72,73 @@ public class LinkedBinaryTree<E> extends AbstractBinaryTree<E> {
         Node<E> node=validate(p);
         return node.getParent();
     }
-    public Position<E> addRoot(E e) throws IllegalArgumentException{
-
+    public Position<E> addRoot(E e) throws IllegalStateException{
+       if(!isEmpty()) throw new IllegalStateException("The tree is not empty");
+       root=createNode(e,null,null,null);
+       size=1;
+       return root;
+    }
+    public Position<E> addLeft(Position<E> p, E e) throws IllegalArgumentException{
+        Node<E> parent = validate(p);
+        if(parent.getLeft()!=null)
+            throw new IllegalArgumentException("p already have the left child");
+        Node<E> child = createNode(e,parent,null,null);
+        parent.setLeft(child);
+        size++;
+        return child;
+    }
+    public Position<E> addRight(Position<E> p, E e) throws IllegalArgumentException{
+        Node<E> parent = validate(p);
+        if(parent.getRight() != null) throw new IllegalArgumentException("p already have the right child");
+        Node<E> child=createNode(e,parent,null,null);
+        parent.setRight(child);
+        size++;
+        return child;
+    }
+    public E set(Position<E> p, E e) throws IllegalArgumentException{
+        Node<E> node= validate(p);
+        E temp=node.getElement();
+        node.setElement(e);
+        return temp;
+    }
+    public void attach(Position<E> p, LinkedBinaryTree<E> T1, LinkedBinaryTree<E> T2) throws IllegalArgumentException{
+        Node<E> node= validate(p);
+        size=size+T1.size()+T2.size();
+        if(isInternal(p)) throw new IllegalArgumentException("The p must be an external/ leaf node");
+        if(!T1.isEmpty()){
+            T1.root.setParent(node);
+            node.setLeft(T1.root);
+            T1.root=null;
+            T1.size=0;
+        }
+        if(!T2.isEmpty()){
+            T2.root.setParent(node);
+            node.setRight(T2.root);
+            T2.root=null;
+            T2.size=0;
+        }
+    }
+    public E remove(Position<E> p) throws IllegalArgumentException{
+        Node<E> node= validate(p);
+        if(numChildren(node)==2) throw new IllegalArgumentException("The p should have only one children for this operation");
+        Node<E> child=(node.getRight()!=null ? node.getRight() : node.getLeft());
+        if(child != null)
+            child.setParent(node.getParent());
+        if(node == root)
+            root=child;
+        else{
+            Node<E> parent=node.getParent();
+            if(parent.getLeft() == node)
+                parent.setLeft(child);
+            else
+                parent.setRight(child);
+        }
+        size--;
+        E temp=node.getElement();
+        node.setElement(null);
+        node.setRight(null);
+        node.setLeft(null);
+        node.setParent(node);
+        return temp;
     }
 }
